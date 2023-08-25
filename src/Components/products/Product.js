20be2e91744200b944292705030a3020cdc69d16
Shahbaz Mirza <weversity.pak@gmail.com>
2023-08-25 12:51:07 +0500

    Added Edit Feature

--- src/Components/products/Product.js
+++ src/Components/products/Product.js
@@ -16,7 +16,7 @@ const customStyles = {
   };
 
 function Product() {
-    
+    const [editID, seteditID] = useState(null);
     const [product, setProduct]=useState(Data);
     const [modalVisible, setModalVisible]=useState(false);
     const [title, setTitle]=useState('');
@@ -41,18 +41,33 @@ function Product() {
         }
         setProduct([addedproducts, ...product])
         setModalVisible(false)
+
     }
 
 
 
-    const ChangeHander =(item)=>{
-        console.log(item);
-        setTitle(item.title);
-        setDescription(item.description);
-        setCategory(item.category);
-        setPrice(item.price);
+    // const ChangeHander =(item)=>{
+    //     console.log(item);
+    //     setTitle(item.title);
+    //     setDescription(item.description);
+    //     setCategory(item.category);
+    //     setPrice(item.price);
         
-        setModalVisible(true);
+    //     setModalVisible(true);
+    // }
+    const changeHandler=()=>{
+
+    };
+
+    const editHandler = (item)=>{
+                seteditID(item.id)
+                setTitle(item.title);
+                setDescription(item.description);
+                setPrice(item.price);
+                setCategory(item.category);
+                setImagURL(item.image)
+                console.log(item);
+                setModalVisible(true)
     }
 
     const deletHander=(id)=>{
@@ -82,7 +97,7 @@ function Product() {
                             <th>{item.description}</th>
                             <th><img style={{width:50}} src={item.image}></img></th>
                             <button onClick={()=>deletHander(item.id)}>Delete</button>
-                            <button onClick={()=>ChangeHander(item)}>Save Changes</button>
+                           <button onClick={()=>editHandler(item)}>Edit</button>
                         </tr>
                     )
                 })}
@@ -91,27 +106,39 @@ function Product() {
             <Modal
                     isOpen={modalVisible}
                     onAfterOpen={()=>null}
-                    onRequestClose={()=>null}
+                    onRequestClose={setModalHandler}
                     style={customStyles}
                 >
+        {editID ? 
+        <h2>Edit Your Product</h2>
+        :
         <h2>Add New Product</h2>
+        }
+        
+        
         <button onClick={()=>setModalVisible(false)}>close</button>
         <form>
                 <label for="exampleFormControlInput1" class="form-label">Category: </label>
                 <input value={category} onChange={(e)=>setCategory(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Category Here" />
 
                 <label for="exampleFormControlInput1" class="form-label">Price: </label>
-                <input onChange={(e)=>setPrice(e.target.value)} type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Price Here" />
+                <input value={price} onChange={(e)=>setPrice(e.target.value)} type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Price Here" />
 
                 <label for="exampleFormControlInput1" class="form-label">Title: </label>
-                <input onChange={(e)=>setTitle(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Title Here" />
+                <input value={title} onChange={(e)=>setTitle(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Title Here" />
 
                 <label for="exampleFormControlInput1" class="form-label">Description: </label>
-                <input onChange={(e)=>setDescription(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Description Here" />
+                <input value={description} onChange={(e)=>setDescription(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Description Here" />
 
                 <label for="exampleFormControlInput1" class="form-label">Image: </label>
-                <input onChange={(e)=>setImagURL(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Image URL Here" />
-          <button onClick={submitHandler} style={{marginTop:10}} type="button" class="btn btn-primary">Submit</button>
+                <input value={imageURL} onChange={(e)=>setImagURL(e.target.value)} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Image URL Here" />
+                {/* {(conditoin) ? first argument: 2nd argument} */}
+                {editID ? 
+                <button onClick={changeHandler} style={{marginTop:10}} type="button" class="btn btn-primary">Save Changes</button>
+                :
+                <button onClick={submitHandler} style={{marginTop:10}} type="button" class="btn btn-primary">Submit</button>
+                }
+                
           
           
           
